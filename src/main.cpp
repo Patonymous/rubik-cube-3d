ce4402ddafbaa027d637e877c54a2972fef7d882
Patonymous <patonymous@gmail.com>
2022-08-21 21:13:55 +0200

    Added rotations

--- src/main.cpp
+++ src/main.cpp
@@ -174,8 +174,232 @@ Tile FindFlatMouse(float mouse_x, float mouse_y, float x, float y, float width, 
         return Tile(Back, (relative_y/side_heigth-1)*SIDE_LENGTH, (relative_x/side_width-3)*SIDE_LENGTH);
 }
 
+inline void Rotate(Cube& cube, Sides side, bool clockwise)
+{
+    if (clockwise)
+    {
+        auto temp = cube[side][0][0];
+        cube[side][0][0] = cube[side][2][0];
+        cube[side][2][0] = cube[side][2][2];
+        cube[side][2][2] = cube[side][0][2];
+        cube[side][0][2] = temp;
+        temp = cube[side][1][0];
+        cube[side][1][0] = cube[side][2][1];
+        cube[side][2][1] = cube[side][1][2];
+        cube[side][1][2] = cube[side][0][1];
+        cube[side][0][1] = temp;
+    }
+    else
+    {
+        auto temp = cube[side][0][0];
+        cube[side][0][0] = cube[side][0][2];
+        cube[side][0][2] = cube[side][2][2];
+        cube[side][2][2] = cube[side][2][0];
+        cube[side][2][0] = temp;
+        temp = cube[side][0][1];
+        cube[side][0][1] = cube[side][1][2];
+        cube[side][1][2] = cube[side][2][1];
+        cube[side][2][1] = cube[side][1][0];
+        cube[side][1][0] = temp;
+    }
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough="
+
+void MovePitch(Cube& cube, Tile where, bool up)
+{
+    switch (where.side)
+    {
+    case Back:
+        where.column = SIDE_LENGTH-where.column-1;
+        up = !up;
+    case Top:
+    case Front:
+    case Bottom:
+        if (up)
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Top][i][where.column];
+                cube[Top][i][where.column] = cube[Front][i][where.column];
+                cube[Front][i][where.column] = cube[Bottom][i][where.column];
+                cube[Bottom][i][where.column] = cube[Back][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1];
+                cube[Back][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1] = temp;
+            }
+            if (where.column == 0)
+                Rotate(cube,Left,false);
+            if (where.column == 2)
+                Rotate(cube,Right,true);
+        }
+        else
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Top][i][where.column];
+                cube[Top][i][where.column] = cube[Back][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1];
+                cube[Back][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1] = cube[Bottom][i][where.column];
+                cube[Bottom][i][where.column] = cube[Front][i][where.column];
+                cube[Front][i][where.column] = temp;
+            }
+            if (where.column == 0)
+                Rotate(cube,Left,true);
+            if (where.column == 2)
+                Rotate(cube,Right,false);
+        }
+        break;
+    case Right:
+        where.column = SIDE_LENGTH-where.column-1;
+        up = !up;
+    case Left:
+        if (up)
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Top][where.column][SIDE_LENGTH-i-1];
+                cube[Top][where.column][SIDE_LENGTH-i-1] = cube[Left][i][where.column];
+                cube[Left][i][where.column] = cube[Bottom][SIDE_LENGTH-where.column-1][i];
+                cube[Bottom][SIDE_LENGTH-where.column-1][i] = cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1];
+                cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1] = temp;
+            }
+            if (where.column == 0)
+                Rotate(cube,Back,false);
+            if (where.column == 2)
+                Rotate(cube,Front,true);
+        }
+        else
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Top][where.column][SIDE_LENGTH-i-1];
+                cube[Top][where.column][SIDE_LENGTH-i-1] = cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1];
+                cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.column-1] = cube[Bottom][SIDE_LENGTH-where.column-1][i];
+                cube[Bottom][SIDE_LENGTH-where.column-1][i] = cube[Left][i][where.column];
+                cube[Left][i][where.column] = temp;
+            }
+            if (where.column == 0)
+                Rotate(cube,Back,true);
+            if (where.column == 2)
+                Rotate(cube,Front,false);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void MoveYaw(Cube& cube, Tile where, bool left)
+{
+    switch (where.side)
+    {
+    case Left:
+    case Front:
+    case Right:
+    case Back:
+        if (left)
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Left][where.row][i];
+                cube[Left][where.row][i] = cube[Front][where.row][i];
+                cube[Front][where.row][i] = cube[Right][where.row][i];
+                cube[Right][where.row][i] = cube[Back][where.row][i];
+                cube[Back][where.row][i] = temp;
+            }
+            if (where.row == 0)
+                Rotate(cube,Top,true);
+            if (where.row == 2)
+                Rotate(cube,Bottom,false);
+        }
+        else
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Left][where.row][i];
+                cube[Left][where.row][i] = cube[Back][where.row][i];
+                cube[Back][where.row][i] = cube[Right][where.row][i];
+                cube[Right][where.row][i] = cube[Front][where.row][i];
+                cube[Front][where.row][i] = temp;
+            }
+            if (where.row == 0)
+                Rotate(cube,Top,false);
+            if (where.row == 2)
+                Rotate(cube,Bottom,true);
+        }
+        break;
+    case Bottom:
+        where.row = SIDE_LENGTH-where.row-1;
+        left = !left;
+    case Top:
+        if (left)
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Top][where.row][SIDE_LENGTH-i-1];
+                cube[Top][where.row][SIDE_LENGTH-i-1] = cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.row-1];
+                cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.row-1] = cube[Bottom][SIDE_LENGTH-where.row-1][i];
+                cube[Bottom][SIDE_LENGTH-where.row-1][i] = cube[Left][i][where.row];
+                cube[Left][i][where.row] = temp;
+            }
+            if (where.row == 0)
+                Rotate(cube,Back,true);
+            if (where.row == 2)
+                Rotate(cube,Front,false);
+        }
+        else
+        {
+            for (unsigned i=0; i<SIDE_LENGTH; ++i)
+            {
+                auto temp = cube[Top][where.row][SIDE_LENGTH-i-1];
+                cube[Top][where.row][SIDE_LENGTH-i-1] = cube[Left][i][where.row];
+                cube[Left][i][where.row] = cube[Bottom][SIDE_LENGTH-where.row-1][i];
+                cube[Bottom][SIDE_LENGTH-where.row-1][i] = cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.row-1];
+                cube[Right][SIDE_LENGTH-i-1][SIDE_LENGTH-where.row-1] = temp;
+            }
+            if (where.row == 0)
+                Rotate(cube,Back,false);
+            if (where.row == 2)
+                Rotate(cube,Front,true);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+#pragma GCC diagnostic pop
+
+void Shuffle(Cube& cube)
+{
+    for (unsigned i=0; i<100; ++i)
+    {
+        const unsigned move = rand()%3;
+        const unsigned position = rand()%3;
+        const bool direction = rand()%2;
+        switch (move)
+        {
+        case 0: //Pitch
+            MovePitch(cube, Tile(Top,0,position), direction);
+            break;
+        
+        case 1: //Yaw
+            MoveYaw(cube, Tile(Top,position,0), direction);
+            break;
+
+        case 2: //Roll
+            MoveYaw(cube, Tile(Front,position,0), direction);
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
 int main()
 {
+    srand(time(0));
+
     Cube cube;
     for (unsigned i=0; i<SIDE_LENGTH; ++i)
     {
@@ -191,8 +415,15 @@ int main()
     }
     Tile mouse = Tile::invalid;
 
-    sf::RenderWindow window(sf::VideoMode(WIND_WIDTH, WIND_HEIGHT), "Rubik!");
+    sf::Font font;
+    sf::Text label;
+    font.loadFromFile("C:/Windows/Fonts/courbd.ttf");
+    label.setFillColor(sf::Color::White);
+    label.setCharacterSize(32);
+    label.setFont(font);
+    label.setString("Press Space\nto shuffle!");
 
+    sf::RenderWindow window(sf::VideoMode(WIND_WIDTH, WIND_HEIGHT), "Rubik!");
     while (window.isOpen())
     {
         sf::Event event;
@@ -209,12 +440,42 @@ int main()
                 // cout << mouse.side << ": " << mouse.row << ", " << mouse.column << endl;
                 break;
 
+            case sf::Event::KeyPressed:
+                switch (event.key.code)
+                {
+                case sf::Keyboard::W:
+                    MovePitch(cube, mouse, true);
+                    break;
+                    
+                case sf::Keyboard::A:
+                    MoveYaw(cube, mouse, true);
+                    break;
+
+                case sf::Keyboard::S:
+                    MovePitch(cube, mouse, false);
+                    break;
+
+                case sf::Keyboard::D:
+                    MoveYaw(cube, mouse, false);
+                    break;
+
+                case sf::Keyboard::Space:
+                    Shuffle(cube);
+                    break;
+
+                default:
+                    break;
+                }
+                break;
+
             default:
                 break;
             }
         }
 
         window.clear();
+        label.setPosition(WIND_WIDTH/2+(WIND_WIDTH/2-label.getGlobalBounds().width)/2, (WIND_HEIGHT/3-label.getGlobalBounds().height)/2-label.getLocalBounds().top);
+        window.draw(label);
         DrawFlatCube(window, cube, 0, 0, WIND_WIDTH, WIND_HEIGHT, mouse);
         window.display();
     }
